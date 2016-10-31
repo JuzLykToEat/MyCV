346401bfdd17a0e7136dcb021e7a01aaeb5088c0
Low Yeong Yih <low.yeongyih@hotmail.com>
2016-11-01 02:33:00 +0800

    made changes to CV in JSON format and added some stylesheets

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,7 +2,19 @@ class LandingController < ApplicationController
 require 'json'
 
   def index
-    
+    cv = File.read('app/assets/javascripts/MyCV.json')
+    data_hash = JSON.parse(cv)
+    @name = data_hash['name']
+    @address = data_hash['address']
+    @mobile = data_hash['mobile']
+    @email = data_hash['email']
+    @profile = data_hash['profile']
+    @achievements = data_hash['achievements']
+    @employments = data_hash['employments']
+    @education = data_hash['education']
+    @core_skills = data_hash['core-skills']
+    @interests = data_hash['interests']
+    binding.pry
   end
 
 end
